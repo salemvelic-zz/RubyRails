567146708d5655a3232c8ef401563fbb1e5292c2
Salem <velicsalem@gmail.com>
2018-03-11 11:27:01 +0000

    Implement advanced login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+  
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -20,6 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
+    
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
